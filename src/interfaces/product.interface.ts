ab1ad7cb3b8e1abfe92270241dd3e963d1f495f6
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2023-05-19 05:40:35 -0300

    feat: products pages, filters, styles, product card link and background

---
+++ src/interfaces/product.interface.ts
@@ -0,0 +1,30 @@
+export interface IProductCategory {
+  id: string;
+  name: string;
+}
+
+export interface IproductCharacteristic {
+  id: string;
+  text: string;
+}
+
+export interface IProductAditionalInfo {
+  id: string;
+  text: string;
+}
+
+export interface IProduct {
+  id: string;
+  name: string;
+  price: string;
+  apresentation: string;
+  active: boolean;
+  category: IProductCategory | null;
+  characteristic?: IproductCharacteristic[];
+  additional_info?: IProductAditionalInfo[];
+}
+
+export interface IProductCard {
+  product: IProduct;
+  type: "big" | "small";
+}
--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -7,6 +7,11 @@ import {
 import axios from "axios";
 import { IAuthenticate } from "@/interfaces/authenticate.interface";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import {
+  fetchCategoriesStart,
+  fetchCategoriesFailure,
+  fetchCategoriesSuccess,
+} from "../reducers/categoriesReducer";
 
 export const authenticate = createAsyncThunk(
   "auth/authenticate",
@@ -36,7 +41,7 @@ export const fetchProducts = () => {
       dispatch(fetchProductsStart());
 
       // Faça uma requisição para a API de busca de produtos
-      const response = await axios.get("http://localhost:3001/product");
+      const response = await axios.get(`http://localhost:3001/product`);
       const products = response.data;
 
       // Atualize o estado com a lista de produtos obtida
@@ -44,6 +49,41 @@ export const fetchProducts = () => {
     } catch (error) {
       // Trate erros de busca de produtos, se houver
       dispatch(fetchProductsFailure("Erro ao buscar produtos"));
+    }
+  };
+};
+export const fetchProductsByCategory = (category: string) => {
+  return async (dispatch: any) => {
+    try {
+      // Antes de buscar os produtos, atualize o estado para indicar que o carregamento está em andamento
+      dispatch(fetchProductsStart());
+
+      // Faça uma requisição para a API de busca de produtos
+      const response = await axios.get(
+        `http://localhost:3001/product/?category=${category}`
+      );
+      const products = response.data;
+
+      // Atualize o estado com a lista de produtos obtida
+      dispatch(fetchProductsSuccess(products));
+    } catch (error) {
+      // Trate erros de busca de produtos, se houver
+      dispatch(fetchProductsFailure("Erro ao buscar produtos"));
+    }
+  };
+};
+
+export const fetchCategories = () => {
+  return async (dispatch: any) => {
+    try {
+      dispatch(fetchCategoriesStart());
+
+      const response = await axios.get("http://localhost:3001/category");
+      const categories = response.data;
+
+      dispatch(fetchCategoriesSuccess(categories));
+    } catch (error) {
+      dispatch(fetchCategoriesFailure("Erro ao buscar produtos"));
     }
   };
 };
