2f31dd1b9baf84e3b8da60f858f7926892a8017a
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2023-05-25 21:20:08 -0300

    feat: modal initial config

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Navbar from "./Navbar";
 import Link from "next/link";
+import { useState } from "react";
 import { IProductCard } from "@/interfaces/product.interface";
 import QuantityControlButton from "./QuantityControlButton";
+import ProductsDetailModal from "./ProductsDetailModal";
 
 export default function ProductCard({ product, type }: IProductCard) {
+  const [openModal, setOpenModal] = useState(false);
+
   const containerType = () => {
     if (type === "small") {
       return "flex flex-row justify-start hover:bg-pirates-black-hover transition p-2 rounded-lg m-3 w-32 text-ms";
@@ -14,36 +18,37 @@ export default function ProductCard({ product, type }: IProductCard) {
     }
   };
 
+  const handleOpenModal = () => {
+    setOpenModal(!openModal);
+  };
+
   return (
-    <div>
-      <Link href={`/shop/${product.id}`}>
-        {" "}
-        <div className={containerType()}>
-          <Image
-            src={product.img_url}
-            alt="drink"
-            width={type === "small" ? 100 : 200}
-            height={0}
-            className="rounded"
-          ></Image>
-          <div className="flex flex-col  text-center  w-full">
-            <div className="flex flex-col">
-              <h1 className="text-base w-full">{product.name.toUpperCase()}</h1>
-              <div className="flex">
-                <div className="w-full border-b-2 border-pirates-red m-3" />
-                <span className="text-pirates-red font-imfell">
-                  {product.amount}
-                </span>
-                <div className="w-full border-b-2 border-pirates-red m-3" />
-              </div>
-              <span className="text-pirates-silver font-fredericka ">
-                R$: {product.price}
+    <ProductsDetailModal product={product}>
+      <div className={containerType()} onClick={handleOpenModal}>
+        <Image
+          src={product.img_url}
+          alt="drink"
+          width={type === "small" ? 100 : 200}
+          height={0}
+          className="rounded"
+        ></Image>
+        <div className="flex flex-col  text-center  w-full">
+          <div className="flex flex-col">
+            <h1 className="text-base w-full">{product.name.toUpperCase()}</h1>
+            <div className="flex">
+              <div className="w-full border-b-2 border-pirates-red m-3" />
+              <span className="text-pirates-red font-imfell">
+                {product.amount}
               </span>
+              <div className="w-full border-b-2 border-pirates-red m-3" />
             </div>
+            <span className="text-pirates-silver font-fredericka ">
+              R$: {product.price}
+            </span>
           </div>
         </div>
-      </Link>
+      </div>
       <QuantityControlButton product={product}></QuantityControlButton>
-    </div>
+    </ProductsDetailModal>
   );
 }
---
+++ src/interfaces/ProductDetailModal.interface.ts
@@ -0,0 +1,5 @@
+import { IProduct } from "./product.interface";
+export interface IProductDetailModal {
+  product: IProduct;
+  isOpen: boolean;
+}
