8825cf151defc3d4cf6ca228bf5279f0bafc8824
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2023-06-11 21:16:03 -0300

    feat: user page initial elements

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,28 +1,65 @@
 import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import ProtectedRoute from "@/components/ProtectedRoute";
 
-export default function User() {
+export default function User(): ReactNode {
+  const [page, setPage] = useState<string>("user");
   const { isAuthenticated } = useSelector((state: any) => state.auth);
-  const [isAuth, setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState<boolean>(false);
+
+  interface IUserPages {
+    [key: string]: ReactNode;
+  }
+  const pages: IUserPages = {
+    user: <>user</>,
+    address: <>address</>,
+    card: <>card</>,
+    history: <>history</>,
+  };
 
   useEffect(() => {
     setIsAuth(isAuthenticated);
   }, [isAuthenticated]);
 
+  const handleOptionPage = (pageName: string): void => {
+    setPage(pageName);
+  };
+
   return (
     <>
       <ProtectedRoute>
-        <h1>
-          {isAuth && <div>User page here</div>}
-          <div>
-            <Link href={"/user/address"}>address</Link>
-            <Link href={"/user/creditcard"}>credit card</Link>
-            <Link href={"/user/purchasehistory"}>purchase history</Link>
+        <div className="flex max-w-5xl   m-auto min-h-screen">
+          <div className="flex flex-col items-end  border-r color ">
+            <button
+              className="m-1 p-1 font-fredericka text-xl"
+              onClick={() => handleOptionPage("user")}
+            >
+              User
+            </button>
+            <button
+              className="m-1 p-1 font-fredericka text-xl"
+              onClick={() => handleOptionPage("address")}
+            >
+              address
+            </button>
+            <button
+              className="m-1 p-1 font-fredericka text-xl"
+              onClick={() => handleOptionPage("card")}
+            >
+              card
+            </button>
+            <button
+              className="m-1 p-1 font-fredericka text-xl"
+              onClick={() => handleOptionPage("history")}
+            >
+              history
+            </button>
           </div>
-        </h1>
+
+          <div className=" w-full bg-black">{pages[page]}</div>
+        </div>
       </ProtectedRoute>
     </>
   );
