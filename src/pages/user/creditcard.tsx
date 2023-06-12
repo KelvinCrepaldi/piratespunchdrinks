956bb8f040da4cf71526b5349ac199fe461c0fb1
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2023-06-11 21:16:03 -0300

    feat: user page initial elements

--- src/pages/user/creditcard.tsx
+++
@@ -1,9 +0,0 @@
-import ProtectedRoute from "@/components/ProtectedRoute";
-
-export default function CreditCard() {
-  return (
-    <ProtectedRoute>
-      <h1>Credit card page</h1>
-    </ProtectedRoute>
-  );
-}
