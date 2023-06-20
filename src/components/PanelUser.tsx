60a197fa572806047948bb64f5b4e793c9a301ea
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2023-06-20 06:23:06 -0300

    user and purchase sections styles

--- src/components/PanelUser.tsx
+++ src/components/PanelUser.tsx
@@ -1,3 +1,47 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 export default function PanelUser() {
-  return <></>;
+  const [user, setUser] = useState<any>(null);
+  const getUser = useSelector((state: any) => state.auth.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setUser(getUser);
+  }, [getUser]);
+
+  return (
+    <div className="font-inter">
+      <div>
+        <h1 className="border-b font-inter">User</h1>
+        <ul className="">
+          <li className="flex justify-between">
+            <span>Name: {user?.name}</span>
+            <button>change</button>
+          </li>
+          <li className="flex justify-between">
+            <span>Email: {user?.email}</span>
+            <button>change</button>
+          </li>
+          <li className="flex justify-between">
+            <span>Password: ******...</span>
+            <button>change</button>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h1 className="border-b font-inter">Address</h1>
+        <div className=" bg-slate-500">
+          <p>{user?.address?.address}</p>
+        </div>
+      </div>
+      <div>
+        <h1 className="border-b font-inter">Credit Cards</h1>
+        <div className=" bg-slate-500">
+          <p>{user?.credit_card}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -4,6 +4,9 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import ProtectedRoute from "@/components/ProtectedRoute";
 
+import PanelUser from "@/components/PanelUser";
+import PanelCartHistory from "@/components/PanelCartHistory";
+
 export default function User(): ReactNode {
   const [page, setPage] = useState<string>("user");
   const { isAuthenticated } = useSelector((state: any) => state.auth);
@@ -13,10 +16,8 @@ export default function User(): ReactNode {
     [key: string]: ReactNode;
   }
   const pages: IUserPages = {
-    user: <>user</>,
-    address: <>address</>,
-    card: <>card</>,
-    history: <>history</>,
+    user: <PanelUser />,
+    history: <PanelCartHistory />,
   };
 
   useEffect(() => {
@@ -30,35 +31,24 @@ export default function User(): ReactNode {
   return (
     <>
       <ProtectedRoute>
-        <div className="flex max-w-5xl   m-auto min-h-screen">
-          <div className="flex flex-col items-end  border-r color ">
+        <div className="flex max-w-5xl m-auto min-h-screen ">
+          <div className="flex flex-col items-end  border-r color p-2">
             <button
-              className="m-1 p-1 font-fredericka text-xl"
+              className="m-1 text-xl md:text-2xl font-inter"
               onClick={() => handleOptionPage("user")}
             >
               User
             </button>
+
             <button
-              className="m-1 p-1 font-fredericka text-xl"
-              onClick={() => handleOptionPage("address")}
-            >
-              address
-            </button>
-            <button
-              className="m-1 p-1 font-fredericka text-xl"
-              onClick={() => handleOptionPage("card")}
-            >
-              card
-            </button>
-            <button
-              className="m-1 p-1 font-fredericka text-xl"
+              className="m-1 text-xl md:text-2xl  font-inter"
               onClick={() => handleOptionPage("history")}
             >
-              history
+              Purchases
             </button>
           </div>
 
-          <div className=" w-full bg-black">{pages[page]}</div>
+          <div className=" w-full px-5 md:px-10   py-5 ">{pages[page]}</div>
         </div>
       </ProtectedRoute>
     </>
