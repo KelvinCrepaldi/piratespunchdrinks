66008e89b3b6f5d34d5c7784508ab012ba3ba8fb
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2023-06-28 23:40:57 -0300

    feat: credit cards user redux and components

--- src/components/PanelUser.tsx
+++ src/components/PanelUser.tsx
@@ -1,24 +1,30 @@
 import { deleteAddress, fetchAddresses } from "@/store/actions/addresses";
+import { fetchCreditCards } from "@/store/actions/creditCards";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function PanelUser() {
   const [user, setUser] = useState<any>(null);
   const [addressList, setAddressList] = useState([]);
+  const [creditCardsList, setCreditCardsList] = useState([]);
 
   const getUser = useSelector((state: any) => state.auth.user);
   const token = useSelector((state: any) => state.auth.token);
   const getAdressList = useSelector((state: any) => state.addresses.addresses);
+  const getcreditCardsList = useSelector(
+    (state: any) => state.creditCards.creditCards
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
     setUser(getUser);
     setAddressList(getAdressList);
-  }, [getUser, getAdressList]);
+    setCreditCardsList(getcreditCardsList);
+  }, [getUser, getAdressList, getcreditCardsList]);
 
   useEffect(() => {
     dispatch(fetchAddresses(token));
-    console.log(addressList);
+    dispatch(fetchCreditCards(token));
   }, [dispatch, token]);
 
   const handleDeleteAddress = (id: string) => {
@@ -26,21 +32,19 @@ export default function PanelUser() {
   };
 
   return (
-    <div className="font-inter">
-      <div>
+    <div className="font-inter ">
+      <div className="">
         <h1 className="border-b font-inter">User</h1>
-        <ul className="">
+        <ul className="flex flex-col m-2 p-3 bg-neutral-900">
           <li className="flex justify-between">
-            <span>Name: {user?.name}</span>
-            <button>change</button>
+            <span>
+              Name: <input defaultValue={user?.name}></input>
+            </span>
           </li>
           <li className="flex justify-between">
-            <span>Email: {user?.email}</span>
-            <button>change</button>
-          </li>
-          <li className="flex justify-between">
-            <span>Password: ******...</span>
-            <button>change</button>
+            <span>
+              Email:<input defaultValue={user?.email}></input>{" "}
+            </span>
           </li>
         </ul>
       </div>
@@ -78,8 +82,26 @@ export default function PanelUser() {
       </div>
       <div>
         <h1 className="border-b font-inter">Credit Cards</h1>
-        <div className=" bg-slate-500">
-          <p>{user?.credit_card}</p>
+        <div className="">
+          {creditCardsList.map((creditCard: any) => (
+            <div
+              key={creditCard.id}
+              className="flex flex-col m-2 p-3 bg-neutral-900"
+            >
+              <div className="flex justify-between border-b text-sm mb-4">
+                <span>id: {creditCard.id}</span>
+                <button>delete</button>
+              </div>
+              <div>
+                Address:
+                <span>{creditCard.name}</span>
+              </div>
+              <div className="flex space-x-5">
+                <span>Number: {creditCard.number}</span>
+                <span>Expiration: {creditCard.expiration_date}</span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
