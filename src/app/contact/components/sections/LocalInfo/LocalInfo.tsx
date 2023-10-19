e89e249826224ed4cad6febbb29d225d96ad408f
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2023-10-19 09:38:31 -0300

    refact: contact sections

---
+++ src/app/contact/components/sections/LocalInfo/LocalInfo.tsx
@@ -0,0 +1,53 @@
+const LocalInfo = (): JSX.Element => {
+  return (
+    <section className=" w-full lg:flex lg:gap-2 lg:justify-center lg:px-5">
+      <div className="bg-zinc-100 shadow-md p-10 my-2 lg:max-w-[400px] flex flex-col justify-between">
+        <h4 className="text-black font-inter mb-10">Localização</h4>
+        <div>
+          <p className="text-black font-inter font-extrabold">
+            Pirate{"'"}s Punch
+          </p>
+          <p className="text-black font-inter ">
+            Sede dos Piratas Sedentos! Ilha Tortuga, Mar do Caribe
+          </p>
+        </div>
+      </div>
+
+      <div className="bg-zinc-100 shadow-md p-10 my-2 lg:max-w-[400px] flex flex-col justify-between">
+        <h4 className="text-black font-inter mb-10 ">
+          Horário de funcionamento{" "}
+        </h4>
+        <div>
+          <p className="text-black font-inter">
+            <span className="font-extrabold text-black">
+              Segunda a sexta-feira:
+            </span>{" "}
+            das 10h00 às 23h00
+          </p>
+          <p className="text-black  font-inter ">
+            <span className="font-extrabold text-black">
+              Sábado e domingo:{" "}
+            </span>
+            das 12h00 à 01h00
+          </p>
+        </div>
+      </div>
+
+      <div className="bg-zinc-100 shadow-md p-10 my-2 lg:max-w-[400px] flex flex-col justify-between">
+        <h4 className="text-black font-inter mb-10">Contato</h4>
+        <div>
+          <p className="text-black  font-inter ">
+            <span className="font-extrabold text-black">Telefone: </span>
+            +1-555-PUNCHY
+          </p>
+          <p className="text-black  font-inter ">
+            <span className="font-extrabold text-black">E-mail:</span>
+            info@piratespunch.com
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LocalInfo;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import Image from "next/image";
 
-import piratepunchlogo from "/public/images/piratepunch.png";
 import tortuga from "/public/images/tortuga.jpg";
+import SendMessage from "./components/sections/SendMessage/SendMessage";
+import LocalInfo from "./components/sections/LocalInfo/LocalInfo";
 
 export default function Contact(): JSX.Element {
   return (
-    <main className="md:max-w-5xl m-auto p-5 md:p-16 space-y-10 font-imfell">
-      <div className="flex flex-col md:flex-row justify-center items-center">
+    <main className="relative m-auto  space-y-10 font-imfell">
+      <div className="flex flex-col md:flex-row justify-center items-center p-5 max-w-[1200px] mx-auto">
         <div className=" flex flex-col justify-center items-center  m-auto">
           <div className="text-center">
             <h1 className="text-pirates-red-strong border-b-4 border-pirates-red-strong font-imfell">
@@ -18,38 +19,29 @@ export default function Contact(): JSX.Element {
             </h2>
           </div>
 
-          <p className="text-center text-pirates-gold">
+          <p className="text-center text-black max-w-[800px] text-2xl">
             Yo ho ho, marujos! Se você deseja entrar em contato conosco, aqui
             estão as informações que você procura. Sinta-se à vontade para
             ancorar em nosso lendário refúgio pirata, Tortuga, e nos fazer uma
             visita. Aqui estão os detalhes:
           </p>
         </div>
-        <Image src={piratepunchlogo} alt="pirate punch logo" width={200} />
       </div>
 
-      <ul className="space-y-3">
-        <li>
-          <h5>Endereço da Loja: </h5>
-          <p className="text-black">
-            Pirate{"'"}s Punch - Sede dos Piratas Sedentos! Ilha Tortuga, Mar do
-            Caribe
-          </p>
-        </li>
-        <li>
-          <h5>Horário de Funcionamento:</h5>
-          <p className="text-black">
-            Segunda a sexta-feira: das 10h00 às 23h00 Sábado e domingo: das
-            12h00 à 01h00
-          </p>
-        </li>
-        <li>
-          <h5>Informações de Contato:</h5>
-          <p className="text-black">Telefone: +1-555-PUNCHY</p>
-          <p className="text-black">E-mail: info@piratespunch.com</p>
-        </li>
-        <li>
-          <h5>Como nos Encontrar:</h5>
+      <SendMessage />
+      <LocalInfo />
+
+      <section className="w-full flex flex-col lg:flex-row items-center justify-center max-w-[1200px] mx-auto my-20">
+        <div className="relative aspect-video  w-full lg:w-1/2 ">
+          <Image
+            className=" rounded xl border-4 border-pirates-black-transparent"
+            src={tortuga}
+            alt="pirate punch logo"
+            fill
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className="lg:w-1/2 p-10">
           <p className="text-black">
             A Ilha Tortuga é um paraíso pirata lendário, mas não se preocupe,
             nós o ajudaremos a encontrar o caminho! Ao chegar na ilha, pergunte
@@ -60,31 +52,25 @@ export default function Contact(): JSX.Element {
             inconfundível placa do Pirate{"'"}s Punch, adornada com caveiras e
             ossos cruzados.
           </p>
-        </li>
-      </ul>
-      <div className="relative w-full aspect-video max-w-[1000px] mx-auto">
-        <Image
-          className=" rounded xl border-4 border-pirates-black-transparent "
-          src={tortuga}
-          alt="pirate punch logo"
-          fill
-          style={{ objectFit: "cover" }}
-        />
-      </div>
-
-      <p className="text-center text-pirates-red-strong">
-        Para qualquer pergunta, sugestão ou histórias de aventuras piratas,
-        sinta-se à vontade para nos contatar por telefone ou e-mail. Nossa
-        tripulação de piratas experientes está sempre pronta para ajudá-lo a
-        encontrar a bebida perfeita para o seu paladar. Se preferir nos enviar
-        uma mensagem em uma garrafa, certifique-se de que ela esteja bem vedada
-        e à prova d{"'"}água - não queremos que suas palavras se percam no mar!
-      </p>
-      <p className="text-center text-pirates-red-strong">
-        Então, marujos, enviem-nos uma mensagem ou planejem sua viagem ao Pirate
-        {"'"}s Punch em Tortuga. Estaremos esperando com um caneco de grogue e
-        uma calorosa recepção pirata!
-      </p>
+          <p className="text-black">
+            Para qualquer pergunta, sugestão ou histórias de aventuras piratas,
+            sinta-se à vontade para nos contatar por telefone ou e-mail. Nossa
+            tripulação de piratas experientes está sempre pronta para ajudá-lo a
+            encontrar a bebida perfeita para o seu paladar. Se preferir nos
+            enviar uma mensagem em uma garrafa, certifique-se de que ela esteja
+            bem vedada e à prova d{"'"}água - não queremos que suas palavras se
+            percam no mar!
+          </p>
+        </div>
+      </section>
+      <section className="max-w-[1200px] mx-auto my-20 pb-20">
+        <h3 className="text-center text-pirates-red-strong">
+          Então, marujos, enviem-nos uma mensagem ou planejem sua viagem ao
+          Pirate
+          {"'"}s Punch em Tortuga. Estaremos esperando com um caneco de grogue e
+          uma calorosa recepção pirata!
+        </h3>
+      </section>
     </main>
   );
 }
